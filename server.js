9f7da9ff409c1f8e7aaba969545eb15c9fe3f355
Tyler Chong <tylrchong@gmail.com>
2016-07-06 18:31:07 -1000

    Readded comments

--- server.js
+++ server.js
@@ -1,15 +1,19 @@
 var net = require('net');
 var socket = net.Socket();
 var storage = {};
-
+//Need to create package.json with dependencies.
+/**
+ * Creates a server instance using the net module to be hosted using node. Each socket is in its own scope from different clients.
+ */
 var server = net.createServer( function(socket) {
   var guestName = 'Guest' + Math.floor(Math.random() * 9999999) + 1;
-  sendData(' connected. Welcome!', guestName);
+  sendData(' connected. Say Hello!', guestName);
   console.log(guestName + ' connected.');
   storage[guestName] = socket;
 
   socket.write('You have been successfully connected, Welcome!\nTo change your name, use /name (your_desired_name) :)');
 
+  //On socket end this is triggered
   socket.on('end', function () {
     sendData(' disconnected.', guestName);
     console.log(guestName + ' disconnected.');
@@ -17,11 +21,12 @@ var server = net.createServer( function(socket) {
     delete storage[guestName];
   });
 
+  //Whenever they send data ver terminal, this is triggered. User commands.
   socket.on('data', function (data) {
     if(data[0] === 47) {
       data = data.toString();
       if(data === '/help\n') {
-        socket.write('/name is the only command lol.');
+        socket.write('/name, /list, /status');
       } else if (data.split(' ')[0] === '/name') {
         data = data.split(' ');
         var check = data[1].replace('\n', '');
@@ -34,10 +39,21 @@ var server = net.createServer( function(socket) {
           guestName = data[1].replace('\n', '');
           storage[guestName] = storage[pastName];
           delete storage[pastName];
-          console.log(pastName + ' has changed his name to ' + guestName);
+          console.log(pastName + ' has changed his/her name to ' + guestName);
         }
-      }
-      else {
+      } else if (data === '/list\n') {
+          var userString = '';
+          for(var prop in storage) {
+            userString = userString + prop + '\n';
+          }
+          socket.write(userString);
+        } else if (data === '/status\n') {
+          var size = 0;
+          for(var key in storage) {
+            size++;
+          }
+          socket.write('There are currently ' + size + ' users online.');
+        } else {
         socket.write('Type /help for a list of commands.');
       }
     }
@@ -47,11 +63,16 @@ var server = net.createServer( function(socket) {
     }
   });
 });
+//The port the server is listening on.
 server.listen(6969);
 
+//Whenever the server passes out data, this is triggered. Admin commands.
 process.stdout.on('data',function (data) {
   if(data[0] === 47) {
     data = data.toString();
+    if(data === '/help\n') {
+      console.log('/kick (user)');
+    }
     if(data.split(' ')[0] === '/kick') {
       data = data.split(' ');
       var check = data[1].replace('\n', '');
@@ -63,6 +84,8 @@ process.stdout.on('data',function (data) {
         storage[check].destroy();
         delete storage[check];
       }
+    } else {
+      console.log('Type /help for admin commands.');
     }
   } else {
     sendData(data, 'ADMIN');
